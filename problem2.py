b9cc1c93c11c06e2337fd565a55007f0dadeb24e
typher7 <23716543-typher7@users.noreply.replit.com>
2023-09-17 07:48:48 +0000

    comment modified

--- problem2.py
+++ problem2.py
@@ -4,7 +4,9 @@
 
 pi = 3.146
 radius = float(input("Enter the radius: "))
-#Area of a circle given the radius is given by: pi*(radius**2)
+#Area of a circle given the radius is given by: pi*(radius**2) 
 
 area = pi * (radius**2)
 print("The area of the circle is ", area)
+
+
